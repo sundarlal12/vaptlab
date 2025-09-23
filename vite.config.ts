9209e3a37af14e13ec669b7e8b4e297930619529
achhishiksha <pardhandhamu@gmail.com>
2025-09-24 01:19:10 +0530

    new deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',          // root deploy (vapt.vercel.app). Change to "./" if needed.
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  // build: { outDir: 'dist' } // optional, default is 'dist'
 });
